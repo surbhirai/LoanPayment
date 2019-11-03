a9505009f80a21047ffa1b058afe167a6f011d2a
unknown <surbhi.rai.0801@gmail.com>
2019-11-03 22:41:45 +0530

    Final changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,11 +4,14 @@ import {Button} from 'react-bootstrap'
 
 function Sidebar(props) {
   const inputList = JSON.parse(localStorage.getItem('keyobj'))
-  const buttonList = inputList.map((item, index) => {
-    return(
+  let buttonList = []
+  if (inputList!==null) {
+     buttonList = inputList.map((item, index) => {
+      return(
       <Button id = {index} type="button" class="list-group-item list-group-item-action" variant = "outline-secondary" onClick = {(e) => props.onclick(e, index)} > Loan Amount: ${item.amount } <br/>  Loan Duration: {item.duration} months </Button>
     )
   })
+}
   const displayButtonList = []
   let counter = 0 
   while (counter < buttonList.length && counter < 10) { //displaying the last 10 inputs only
